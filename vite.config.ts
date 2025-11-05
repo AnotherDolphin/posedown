eace4bcba76baccf788cebd17081b37b88954a11
Abdullah Nayer <abdullahnayer@gmail.com>
2025-11-05 19:34:33 +0200

    wip: hast/mdast util for granndual (non-whole tree) ops. next: use new util fncs to handle md inline in dom

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,17 @@ import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+	plugins: [tailwindcss(), sveltekit()],
+	optimizeDeps: {
+		include: [
+			'firebase/app',
+			'firebase/auth',
+			'firebase/firestore',
+			'firebase/storage'
+		],
+		exclude: ['@lucide/svelte']
+	},
+	ssr: {
+		noExternal: ['bits-ui']
+	}
 })
