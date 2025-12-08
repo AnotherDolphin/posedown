6d327e355f167a82d7957799c235af52a765612b
Abdullah Nayer <abdullahnayer@gmail.com>
2025-12-08 07:29:10 +0200

    test: rich editor input

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,9 +1,12 @@
-import { defineConfig } from '@playwright/test';
+import { defineConfig } from '@playwright/test'
 
 export default defineConfig({
 	webServer: {
-		command: 'npm run build && npm run preview',
-		port: 4173
+		// command: 'npm run build && npm run preview',
+		// port: 4173
+		command: 'npm run dev',
+		port: 5173,
+		reuseExistingServer: true
 	},
-	testDir: 'e2e'
-});
+	testDir: 'tests/e2e'
+})
