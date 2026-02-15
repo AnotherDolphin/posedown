0d44a470bdb2ee306c9ddd80f0e338c92fa5cfd0
Abdullah <abdullahnayer@gmail.com>
2026-02-15 09:30:09 +0200

    config: playwright url (wsl supp)

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from '@playwright/test'
 
+const BASE_URL = process.env.BASE_URL ?? 'http://localhost:5173'
+
 export default defineConfig({
+	use: {
+		baseURL: BASE_URL
+	},
 	webServer: {
 		// command: 'npm run build && npm run preview',
 		// port: 4173
 		command: 'npm run dev',
-		port: 5173,
+		url: BASE_URL,
 		reuseExistingServer: true
 	},
 	testDir: 'tests/e2e'
